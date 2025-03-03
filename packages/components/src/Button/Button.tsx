31d74e99ffd12feeef3e7faacf398f5ac58f9dfb
Jhonatan Benavides <jhonatan815@gmail.com>
2025-03-04 00:35:27 +0100

    chore: bump version to 1.0.12 (components)

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
   disabled?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ children, disabled }) => {
+const Button: React.FC<ButtonProps> = ({ children, disabled = false }) => {
   return <Sc.StyledButton disabled={disabled}>{children}</Sc.StyledButton>
 }
 
