886ffddfcc18e61911c427e20e3b66174b0843bf
Jhonatan Benavides <jhonatan815@gmail.com>
2025-03-03 21:29:59 +0100

    chore: settings vite and tsconfig to build

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      outDir: 'dist',
+      insertTypesEntry: true,
+      cleanVueFileName: true,
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: './src/index.ts',
@@ -13,8 +21,7 @@ export default defineConfig({
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // Asegúrate de que aquí no se estén buscando archivos html
-      input: './src/index.ts', // Asegúrate de que no haya index.html como entrada
+      input: './src/index.ts',
       external: ['react'],
     },
   },
