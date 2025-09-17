44e316094868ce296aa86946df137ab0a3ec194f
Jhonatan Benavides <jhonatan815@gmail.com>
2025-09-17 13:22:39 +0200

    chore: add script changeset fix test

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ export interface ButtonProps {
 export const Button = ({ children, appName }: ButtonProps) => {
   return (
     <button
-      className="bg-emerald-700 text-white px-4 py-2 rounded hover:bg-red-400 border-2"
+      className="bg-emerald-700 text-white px-4 py-2 rounded hover:bg-red-400 border-4"
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
